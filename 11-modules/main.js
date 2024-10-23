9fafcee24cf8e06cc95adfdff24c5278248230a6
Pedro Hurtado <pedrohurt@gmail.com>
2024-10-23 08:55:48 +0200

    modules

---
+++ 11-modules/main.js
@@ -0,0 +1,11 @@
+import {sum as suma,resta,multiplicacion,division} from './operations.js'
+import * as op from './operations.js'
+import './run.js'  //ejecucion y no importacion
+import Pagina1 from './pages/page1.js'
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export
+
+Pagina1()
+
+console.log(suma(5,3))
+console.log(op.multiplicacion(5,3))
